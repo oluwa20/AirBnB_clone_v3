2e53009897efc2a400f957f0abab902e3b8b44ac
Dalvin Onyango <dalvin.onyango@students.uonbi.ac.ke>
2024-05-27 21:16:44 +0300

    storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -114,8 +114,7 @@ class TestFileStorage(unittest.TestCase):
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
 
-
-        @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
     def test_get(self):
         """Test that get properly returns a requested object"""
         storage = FileStorage()
